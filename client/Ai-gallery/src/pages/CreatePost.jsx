93fc98d36ab488437ce19da93ac9b7b91ee6e0a3
Gabriel Alves <gabrielsoaresalves@gmail.com>
2023-01-25 12:24:26 -0300

    new backend routes

--- client/Ai-gallery/src/pages/CreatePost.jsx
+++ client/Ai-gallery/src/pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-efk8.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/posts', {
+        const response = await fetch('https://dall-e-efk8.onrender.com/api/v1/posts', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/Ai-gallery/src/pages/Home.jsx
+++ client/Ai-gallery/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/posts', {
+      const response = await fetch('https://dall-e-efk8.onrender.com/api/v1/posts', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
